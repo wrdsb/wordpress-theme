f7c92deb64893c3eab74ac3d55e3de0f3e9daeeb
James Schumann <jschumann@gmail.com>
2014-06-20 14:19:46 -0400

    added right sidebar and right menu to home.php

--- home.php
+++ home.php
@@ -26,47 +26,10 @@
 
           </div>
           <div class="col-sm-4">
-            <!--
-            <h2><strong>Events</strong></h2>
-
-            <div class="event">
-              <div class="when">
-                <div class="month">FEB</div>
-                <div class="date">26</div>
-                <div class="day">Wed</div>
-              </div>
-              <div class="what">
-                <span>6:00 pm</span> Westmount PS Community Meeting
-              </div>
-              <div class="clear"></div>
-            </div>
-
-            <div class="event">
-              <div class="when">
-                <div class="month">MAR</div>
-                <div class="date">7</div>
-                <div class="day">Fri</div>
-              </div>
-              <div class="what">
-                <span class="all-day"></span> PD Day
-              </div>
-              <div class="clear"></div>
-            </div>
-
-            <div class="event">
-              <div class="when">
-                <div class="month">MAR</div>
-                <div class="date">10</div>
-                <div class="day">Mon</div>
-              </div>
-              <div class="what">
-                <span class="all-day"></span> March Break
-              </div>
-              <div class="clear"></div>
-            </div>
-
-            <p class="text-right"><button type="button" class="btn btn-xs mybutton">View Calendar <span class="glyphicon glyphicon-arrow-right"></span></button></p>
-            -->
+            <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')): ?>
+              <?php get_sidebar('rmenu'); ?>
+              <?php get_sidebar('right'); ?>
+            <?php endif ?>
           </div>
         </div>
       </div>
