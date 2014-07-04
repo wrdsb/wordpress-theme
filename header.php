870f583d3afc235c7ad58bf4adcc1976c5ec81a4
James Schumann <jschumann@gmail.com>
2014-07-04 10:39:10 -0400

    now showing page title and site name in title. closes #36.

--- header.php
+++ header.php
@@ -13,7 +13,11 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php bloginfo('name'); ?></title>
+  <?php if (is_front_page()) { ?> 
+    <title><?php bloginfo('name'); ?></title>
+  <?php } else { ?>
+    <title><?php wp_title(''); ?> (<?php bloginfo('name'); ?>)</title>
+  <?php } ?>
 
   <!-- Bootstrap -->
   <link href="http://spectro.wrdsb.ca/css/bootstrap.css" rel="stylesheet">
