5f94bedb2f5f4210b69182f601bc9f4439f25867
James Schumann <jschumann@gmail.com>
2014-07-16 16:27:28 -0400

    default page menu fixed

--- sidebar-lmenu.php
+++ sidebar-lmenu.php
@@ -17,43 +17,90 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-
     <span class="navbar-brand">Subnav</span>
   </div>
-  <div class="collapse navbar-collapse sub-navbar-collapse">
-    <div class="sub-menu-heading">
-      <span><?php echo get_the_title(get_post_top_ancestor_id()); ?></span>
-    </div>
-    <div class="sub-menu-items">
-      <?php if (has_nav_menu('left')) {
-        wp_nav_menu(array('theme_location' => 'left', 'menu_class' => '', 'container' => false));
-      } else {
-        #$ancestor_id=get_post_top_ancestor_id();
-        #$descendants = get_pages(array('child_of' => $ancestor_id));
-        #$incl = "";
 
-        #foreach ($descendants as $page) {
-          #if (($page->post_parent == $ancestor_id) ||
-              #($page->post_parent == $post->post_parent) ||
-              #($page->post_parent == $post->ID)) {
-            #$incl .= $page->ID . ",";
-          #}
-        #}
-        #echo '<ul>';
-        #wp_list_pages(array("child_of" => $ancestor_id, "include" => $incl, "link_before" => "", "title_li" => "", "sort_column" => "menu_order"));
-        #echo '</ul>';
-if (!$post->post_parent) {
-  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-}else{
-  if($post->ancestors) {
-    $ancestors = end($post->ancestors);
-    $children = wp_list_pages("title_li=&child_of=".$ancestors."&echo=0");
-  }
-}
-if ($children) {
-  echo '<ul>'.$children.'</ul>';
-}
-      }
+  <?php # If we have a menu in the 'left' menu location ... ?>
+  <?php if (has_nav_menu('left')): ?>
+    <div class="collapse navbar-collapse sub-navbar-collapse">
+      <div class="sub-menu-heading">
+        <?php $menu_locations = get_nav_menu_locations(); ?>
+        <?php $menus = wp_get_nav_menus(); ?>
+        <?php foreach ($menus as $menu) {
+          if ($menu->term_id == $menu_locations['left']) {
+            echo '<span>'.$menu->name.'</span>';
+          }
+        } ?>
+      </div>
+      <div class="sub-menu-items">
+        <?php wp_nav_menu(array('theme_location' => 'left', 'menu_class' => '', 'container' => false)); ?>
+      </div>
     </div>
-  </div>
+
+  <?php # Else we display a pages menu by default ?>
+  <?php else:
+    $exclude = "";
+    $siblings = get_pages(array(
+      'parent' => $post->post_parent,
+      'exclude' => $post->ID, 
+      'post_type' => 'page',
+    ));
+    $children = get_pages(array(
+      'sort_order' => 'ASC',
+      'sort_column' => 'menu_order',
+      'parent' => $post->ID,
+      'post_type' => 'page',
+    ));
+  ?>
+    <?php if ($post->post_parent != 0) { ?>
+      <div class="collapse navbar-collapse sub-navbar-collapse">
+        <div class="sub-menu-heading">
+          <span><a href="<?php echo get_permalink($post->post_parent); ?>"><?php echo get_the_title($post->post_parent); ?></a></span>
+        </div>
+        <div class="sub-menu-items">
+          <ul>
+            <?php 
+              foreach ($siblings as $sibling) {
+                # Get children of sibling
+                $nieces = get_pages(array(
+                  'parent' => $sibling->ID
+                ));
+                # Exclude those children
+                foreach ($nieces as $niece) {
+                  $exclude .= $niece->ID . ",";
+                }
+              }
+              wp_list_pages(array(
+                "child_of" => $post->post_parent,
+                "exclude" => $exclude,
+                "link_before" => "",
+                "title_li" => "",
+                "sort_column" => "menu_order"
+              ));
+            ?>
+          </ul>
+        </div>
+      </div>
+    <?php } elseif (($post->post_parent == 0) and ($children)) { ?>
+      <div class="collapse navbar-collapse sub-navbar-collapse">
+        <div class="sub-menu-heading">
+          <span><a href="<?php echo get_permalink($post->post_parent); ?>"><?php echo get_the_title($post->post_parent); ?></a></span>
+        </div>
+        <div class="sub-menu-items">
+          <ul>
+            <?php
+              wp_list_pages(array(
+                "child_of" => $post->ID,
+                "depth" => 1,
+                "link_before" => "",
+                "title_li" => "",
+                "sort_column" => "menu_order"
+              ));
+            ?>
+          </ul>
+        </div>
+      </div>
+    <?php } ?>
+
+  <?php endif ?>
 </div> <!-- end navigation -->
