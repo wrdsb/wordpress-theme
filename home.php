bbb4fa182ae0a37284e9d3b400a0e9135f58b94f
James Schumann <jschumann@gmail.com>
2014-07-16 10:49:50 -0400

    added all sidebar possibilities to home.php

--- home.php
+++ home.php
@@ -2,7 +2,58 @@
 
       <div class="container">
         <div class="row">
-          <div class="col-sm-8">
+
+    <?php $has_left = FALSE; ?>
+    <?php $has_right = FALSE; ?>
+    <?php if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;} ?>
+    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;} ?>
+
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
+      if (!is_front_page()) {
+        get_sidebar('lmenu');
+      }
+      get_sidebar('left');
+      echo '</div>';
+
+    # Just left sidebar
+    elseif (($has_left == TRUE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-3 col-lg-2">';
+      if (!is_front_page()) {
+        get_sidebar('lmenu');
+      }
+      get_sidebar('left');
+      echo '</div>';
+
+    # Just right sidebar
+      # Nothing to do
+
+    # No sidebars
+      # Nothing to do
+    endif
+    ?>
+
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-6 col-md-7 col-lg-8">';
+
+    # Just left sidebar
+    elseif (($has_left == TRUE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-9 col-lg-10">';
+
+    # Just right sidebar
+    elseif (($has_left == FALSE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-8">';
+
+    # No sidebars
+    elseif (($has_left == FALSE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-12 col-lg-12">';
+
+    endif
+    ?>
 
             <h2>News & Announcements</h2>
             <?php
@@ -24,13 +75,35 @@
               wrdsb_paging_nav();
             ?>
 
-          </div>
-          <div class="col-sm-4">
-            <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')): ?>
-              <?php get_sidebar('rmenu'); ?>
-              <?php get_sidebar('right'); ?>
-            <?php endif ?>
-          </div>
+    </div> <!-- end content area -->
+
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
+      if (!is_front_page()) {
+        get_sidebar('lmenu');
+      }
+      get_sidebar('left');
+      echo '</div>';
+
+    # Just left sidebar
+      # Nothing to do
+
+    # Just right sidebar
+    elseif (($has_left == TRUE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-4"">';
+      if (!is_front_page()) {
+        get_sidebar('rmenu');
+      }
+      get_sidebar('right');
+      echo '</div>';
+
+    # No sidebars
+      # Nothing to do
+
+    endif
+    ?>
         </div>
       </div>
 
