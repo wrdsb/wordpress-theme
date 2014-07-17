9dcd9ffa429d42605ce18abc395fb822089c1e3e
rhkiyani <kiyani.rizwan@gmail.com>
2014-07-17 12:26:58 -0400

    responsive table

--- footer.php
+++ footer.php
@@ -74,6 +74,11 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
       <!-- Include all compiled plugins (below), or include individual files as needed -->
       <script src="http://spectro.wrdsb.ca/js/bootstrap.min.js"></script>
+
+      <script type="text/JavaScript">
+        $("table").addClass("table table-striped table-bordered");
+        $("table").wrap("<div class='table-responsive'></div>");
+      </script>
     <?php wp_footer(); ?>
     </body>
     </html>
