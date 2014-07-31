b74d224ac868ef0acd7a4e5943b73346e994b2d9
rhkiyani <kiyani.rizwan@gmail.com>
2014-07-31 10:02:55 -0400

    update on single.php
    removed left sidebar

--- single.php
+++ single.php
@@ -2,54 +2,12 @@
 
 <div class="container">
   <div class="row">
-
-    <?php $has_left = FALSE; ?>
-    <?php $has_right = FALSE; ?>
-    <?php if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;} ?>
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;} ?>
-    <?php if (!is_front_page()) {$has_left = TRUE;} ?>
-
-    <?php
-    # Both sidebars
-    # left column
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-    if (!is_front_page()) {
-      get_sidebar('lmenu');
-    }
-    get_sidebar('left');
-    echo '</div>';
-
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-3 col-lg-3">';
-    if (!is_front_page()) {
-      get_sidebar('lmenu');
-    }
-    get_sidebar('left'); 
-    echo '</div>';
-
-    # Just right sidebar
-      # Nothing to do
-
-    # No sidebars
-      # Nothing to do
-    endif;
-    ?>
-
     <?php
     # Both sidebars
     # content area
-    if (($has_left == TRUE) and ($has_right == TRUE)):
+    if ($has_right == TRUE):
       echo '<div class="col-sm-6 col-md-8 col-lg-8">';
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-9 col-lg-9">';
-    # Just right sidebar
-    elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-8">';
-    # No sidebars
-    elseif (($has_left == FALSE) and ($has_right == FALSE)):
+    elseif ($has_right == FALSE):
       echo '<div class="col-sm-12 col-lg-12">';
     endif;
     ?>
@@ -103,9 +61,8 @@
   <?php endwhile; else: ?>
   <p> <?php _e('Sorry, no posts matched your criteria.'); ?> </p>
 <?php endif; ?>
-<p><?php previous_post_link('&laquo; Previous Post: %link'); ?> <?php if(!get_adjacent_post(false, '', true)) { echo ''; } // if there are no older articles ?>
-  <span style="margin:0 10px;"></span>
-<?php next_post_link('Next Post: %link &raquo;'); ?> <?php if(!get_adjacent_post(false, '', false)) { echo ''; } // if there are no newer articles ?> </p>
+<p><?php previous_post_link('&laquo; Previous Post: %link'); ?> <?php if(!get_adjacent_post(false, '', true)) { echo ''; } // if there are no older articles ?></p>
+<p><?php next_post_link('Next Post: %link &raquo;'); ?> <?php if(!get_adjacent_post(false, '', false)) { echo ''; } // if there are no newer articles ?> </p>
 
         <?php edit_post_link(__('<strong>Edit</strong>'));?>
       </div>
@@ -117,30 +74,14 @@
 </div> <!-- end content area -->
 
 <?php
-    # Both sidebars
-    # right column
-if (($has_left == TRUE) and ($has_right == TRUE)):
-  echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-if (!is_front_page()) {
-  get_sidebar('rmenu');
-}
-get_sidebar('right');
-echo '</div>';
-
-    # Just left sidebar
-      # Nothing to do
-
-    # Just right sidebar
-elseif (($has_left == FALSE) and ($has_right == TRUE)):
+    
+if ($has_right == TRUE):
   echo '<div class="col-sm-4">';
 if (!is_front_page()) {
   get_sidebar('rmenu');
 }
 get_sidebar('right');
 echo '</div>';
-
-    # No sidebars
-      # Nothing to do
 
 endif;
 ?>
