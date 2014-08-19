fef1fa736e8b2aede12aabd4e6a2cf24a4ded6de
James Schumann <jschumann@gmail.com>
2014-08-19 13:51:10 -0400

    added conditionals for school sites

--- home.php
+++ home.php
@@ -13,7 +13,7 @@
     # left column
     if (($has_left == TRUE) and ($has_right == TRUE)):
       echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-      if (is_front_page()) {
+      if (is_front_page() && wrdsb_i_am_a_school()) {
         echo '<a href="http://myway.wrdsb.ca"><img src="http://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a>';
         echo '<a href="/about/school-year-information"><img src="http://www.wrdsb.ca/wp-content/uploads/schoolyearinformation.jpg" alt="School Year Information Logo"/></a>';
       }
@@ -26,7 +26,7 @@
     # Just left sidebar
     elseif (($has_left == TRUE) and ($has_right == FALSE)):
       echo '<div class="col-sm-3 col-lg-3">';
-      if (is_front_page()) {
+      if (is_front_page() && wrdsb_i_am_a_school()) {
         echo '<a href="http://myway.wrdsb.ca"><img src="http://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a>';
         echo '<a href="/about/school-year-information"><img src="http://www.wrdsb.ca/wp-content/uploads/schoolyearinformation_344x100.jpg" alt="School Year Information Logo"/></a>';
       }
@@ -107,6 +107,10 @@
     # Just right sidebar
     elseif (($has_left == FALSE) and ($has_right == TRUE)):
       echo '<div class="col-sm-4">';
+      if (is_front_page() && wrdsb_i_am_a_school()) {
+        echo '<a href="http://myway.wrdsb.ca"><img src="http://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a>';
+        echo '<a href="/about/school-year-information"><img src="http://www.wrdsb.ca/wp-content/uploads/schoolyearinformation_344x100.jpg" alt="School Year Information Logo"/></a>';
+      }
       if (!is_front_page()) {
         get_sidebar('rmenu');
       }
