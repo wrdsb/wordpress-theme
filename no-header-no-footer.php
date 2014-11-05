145de5e6c332944748157645fa483be7b7a1be6b
rhkiyani <kiyani.rizwan@gmail.com>
2014-11-05 13:47:22 -0500

    new page template
    no header no footer...for hr forms

---
+++ no-header-no-footer.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: No Sidebars
+*/
+?>
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <?php // check if the post has a Post Thumbnail assigned to it.
+        if ( has_post_thumbnail() ) {
+	  the_post_thumbnail('wrdsb-full-width');
+        }
+      ?>
+      <?php
+        // Start the Loop.
+        while ( have_posts() ) : the_post();
+
+          // Include the page content template.
+          get_template_part( 'content', 'page' );
+
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) {
+            comments_template();
+          }
+        endwhile;
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
