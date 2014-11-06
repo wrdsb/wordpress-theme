712dc66a52b78933cc9ca981e470bcc387b59253
rhkiyani <kiyani.rizwan@gmail.com>
2014-11-06 09:41:24 -0500

    New page template No Header-No Footer

--- no-header-no-footer.php
+++ no-header-no-footer.php
@@ -1,18 +1,48 @@
 <?php
 /*
-Template Name: No Sidebars
+Template Name: No Header/Footer
 */
 ?>
-<?php get_header(); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php if (is_front_page()) { ?> 
+    <title><?php bloginfo('name'); ?></title>
+  <?php } else { ?>
+    <title><?php wp_title(''); ?> (<?php bloginfo('name'); ?>)</title>
+  <?php } ?>
+
+  <style type="text/css">
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #333;
+    background-color: #fff;
+    margin: 0;
+  }
+.container {
+  width:870px;
+  margin:0 auto;
+  padding:15px;
+}
+.row {
+  display:block;
+}
+h1 {
+  display:none;
+}
+  </style>
+</head>
+<body>
 <div class="container">
   <div class="row">
     <div class="col-sm-12">
-      <?php // check if the post has a Post Thumbnail assigned to it.
-        if ( has_post_thumbnail() ) {
-	  the_post_thumbnail('wrdsb-full-width');
-        }
-      ?>
+      
       <?php
         // Start the Loop.
         while ( have_posts() ) : the_post();
@@ -21,13 +51,12 @@ Template Name: No Sidebars
           get_template_part( 'content', 'page' );
 
           // If comments are open or we have at least one comment, load up the comment template.
-          if ( comments_open() || get_comments_number() ) {
-            comments_template();
-          }
+          
         endwhile;
       ?>
     </div>
   </div>
 </div>
+</body>
+</html>
 
-<?php get_footer(); ?>
