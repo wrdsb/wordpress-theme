0be208721e2059cbf0afa770259c52c0e2198523
Suzanne <suzannezc@users.noreply.github.com>
2014-12-16 10:45:06 -0500

    Adjusting categories and tags

--- content.php
+++ content.php
@@ -49,20 +49,20 @@ endif;
 	}
 
 	if ($igc == 1) {
-		$display_cats = the_category(' &bull; ');
+		$display_cats = 1;
 	}
 
 	$number_of_tags = count(get_terms('post_tags'));
 
 	if ($number_of_tags > 0) {
-		$display_tags = the_tags('',' &bull; ','');
+		$display_tags = 1;
 	}
 
 	if (!isset($display_cats) && isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Tags: ' . $display_tags . '</p>';
+		echo '<p class="categories gray-dark small">Tags: ' . the_tags('',' &bull; ','') . '</p>';
 	} elseif (isset($display_cats) && !isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Categories: ' . $display_cats . '</p>';
+		echo '<p class="categories gray-dark small">Categories: ' . the_category(' &bull; ') . '</p>';
 	} elseif (isset($display_cats) && isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Categories: ' . $display_cats . ' Tags: ' . $display_tags . '</p>';
+		echo '<p class="categories gray-dark small">Categories: ' . the_category(' &bull; ') . ' Tags: ' . the_tags('',' &bull; ','') . '</p>';
 	}
 ?>
