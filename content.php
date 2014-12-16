f59509edc713a8752511a510f15f182cebfa26ba
James Schumann <jschumann@gmail.com>
2014-12-16 11:00:05 -0500

    tags label iff tags

--- content.php
+++ content.php
@@ -52,17 +52,26 @@ endif;
 		$display_cats = 1;
 	}
 
-	$number_of_tags = count(get_terms('post_tags'));
+	$number_of_tags = count(get_the_tags());
 
-	if ($number_of_tags > 0) {
+	//if ($number_of_tags > 0) {
+	if (get_the_tags()) {
 		$display_tags = 1;
 	}
 
 	if (!isset($display_cats) && isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Tags: ' . the_tags('',' &bull; ','') . '</p>';
+		echo '<p class="categories gray-dark small">Tags: ';
+                the_tags('',' &bull; ','');
+                echo '</p>';
 	} elseif (isset($display_cats) && !isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Categories: ' . the_category(' &bull; ') . '</p>';
+		echo '<p class="categories gray-dark small">Categories: ';
+                the_category(' &bull; ');
+                echo '</p>';
 	} elseif (isset($display_cats) && isset($display_tags)) {
-		echo '<p class="categories gray-dark small">Categories: ' . the_category(' &bull; ') . ' Tags: ' . the_tags('',' &bull; ','') . '</p>';
+		echo '<p class="categories gray-dark small">Categories: ';
+                the_category(' &bull; ');
+                echo ' Tags: ';
+                the_tags('',' &bull; ','');
+                echo '</p>';
 	}
 ?>
