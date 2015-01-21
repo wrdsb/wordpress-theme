aef3dac8185d0c2da2b35cbcdc372241a66e1024
Suzanne <suzannezc@users.noreply.github.com>
2015-01-21 10:36:10 -0500

    rolling back the school link change

--- footer.php
+++ footer.php
@@ -63,15 +63,7 @@
             </div>
             <div class="col-sm-4">
               <p class="copyright">
-                  <?php 
-                  $parsed_url = parse_url(network_site_url());
-                  $host = explode('.', $parsed_url['host']);
-                  if ($host[0] == 'schools') { ?>
-                        <a target="_blank" href="http://schools.wrdsb.ca/<?php $fulldomain = explode('.',$_SERVER['HTTP_HOST']); echo $fulldomain[0]; ?>/wp-login.php">Log in</a>
-                  <?php } else {
-                  wp_loginout(); 
-                  }?>
-                
+                wp_loginout(); 
                 &nbsp;&nbsp; | &nbsp;&nbsp;
                 <a href="http://staff.wrdsb.ca">Log in to Staff Website</a>
                 <?php 
