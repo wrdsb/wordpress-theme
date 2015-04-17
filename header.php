3bbf1395ff8bbbd89fa94ede01151c18e103e13d
James Schumann <jschumann@gmail.com>
2015-04-17 10:26:18 -0400

    syntax error fix in header.php

--- header.php
+++ header.php
@@ -74,7 +74,7 @@
 
   <?php wrdsb_secondary_school_colours(); ?>
 
-  <?php if wrdsb_i_am_a_school() { ?>
+  <?php if (wrdsb_i_am_a_school()) { ?>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
