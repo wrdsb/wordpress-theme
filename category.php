9bdff6cae6c7e7695e03785ffbb2d3c933be3c37
Suzanne <suzannezc@users.noreply.github.com>
2015-05-25 16:08:31 -0400

    Update category.php

--- category.php
+++ category.php
@@ -46,17 +46,15 @@
     endif
     ?>
 
-<?php if ( $paged < 2 ) { // Do stuff specific to first page?>
-<?php if ( is_category() ) : ?>
-<h2 id="category-name-header"><?php echo $cache_categories[$cat]->cat_name ?></h2>
-<?php add_filter('category_description', 'wpautop'); ?>
-<?php add_filter('category_description', 'wptexturize'); ?>
-<div id="category-description">
-<?php echo category_description(); ?>
-</div>
-<?php endif; ?>
-<?php } else { // Do stuff specific to non-first page ?>
-<?php } ?>
+<?php
+	if ( is_category() ) : ?>
+	<h2 id="category-name-header"><?php echo $cache_categories[$cat]->cat_name ?></h2>
+	<?php add_filter('category_description', 'wpautop'); ?>
+	<?php add_filter('category_description', 'wptexturize'); ?>
+	<div id="category-description">
+	<?php echo category_description(); ?>
+	</div>
+	<?php endif; ?>
 
            <?php
               // Start the Loop.
