9771ab51a191a3e1604f671296793176ea5eed00
James Schumann <jschumann@gmail.com>
2015-08-27 13:11:20 -0400

    updated staff list template to match staff list plugin

--- staff_list.php
+++ staff_list.php
@@ -3,90 +3,113 @@
 Template Name: Staff List
 */
 ?>
+<?php $user_query = new WP_User_Query(array('meta_key' => 'wrdsb_display_in_staff_list', 'meta_value' => 1)); ?>
 <?php get_header(); ?>
 
 <div class="container">
-	<div class="row">
-		<div class="col-md-12">
+  <div class="row">
+    <div class="col-md-12">
+    <?php
+      // Start the content loop.
+      while ( have_posts() ) : the_post();
+        // Include the post format-specific content template.
+        get_template_part( 'content', 'page' );
+      endwhile;
 
-        <?php
-          // Start the Loop.
-          while ( have_posts() ) : the_post();
+      // User Loop
+      if (!empty($user_query->results)) { ?>
+        <div class="table-responsive hidden-sm" >
+          <table class="table table-striped table-bordered table-fixed-head" data-toggle="table" data-sort-name="role">
+            <thead>
+              <tr>
+                <th class="text-left" data-field="name" data-sortable="true">Name</th>
+                <th class="text-left" data-field="role" data-sortable="true">Role</th>
+                <th class="text-left" data-field="email" data-sortable="true">Email</th>
+                <th class="text-left" data-field="voicemail" data-sortable="true">Voicemail</th>
+                <th class="text-left" data-field="website" data-sortable="true">Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach($user_query->results as $user) { ?>
+                <tr>
+                  <td><?php echo $user->last_name; ?>, <?php echo $user->first_name; ?></td>
+                  <td>
+                    <span class="hidden">
+                    <?php switch($user->wrdsb_job_title) {
+                      case 'Principal':
+                        echo '1';
+                        break;
+                      case 'Vice-Principal':
+                        echo '2';
+                        break;
+                      case 'Office Manager':
+                        echo '10';
+                        break;
+                      case 'Head Secretary':
+                        echo '11';
+                        break;
+                      case 'Secretary':
+                        echo '12';
+                        break;
+                      case 'Department Head':
+                        echo '50';
+                        break;
+                      case 'Teacher':
+                        echo '51';
+                        break;
+                      case 'Educational Assistant':
+                        echo '70';
+                        break;
+                      case 'Custodian':
+                        echo '80';
+                        break;
+                      default:
+                        echo '99';
+                    } ?>
+                    </span>
+                    <?php echo $user->wrdsb_job_title; ?>
+                  </td>
+                  <?php if ($user->wrdsb_contact_options == 'Email') { ?>
+                    <td><?php echo $user->user_email; ?></td>
+                    <td>&nbsp;</td>
+                  <?php } elseif ($user->wrdsb_contact_options == 'Voicemail') { ?>
+                    <td>&nbsp;</td>
+                    <td><?php echo $user->wrdsb_voicemail; ?></td>
+                  <?php } else { ?>
+                    <td><?php echo $user->user_email; ?></td>
+                    <td><?php echo $user->wrdsb_voicemail; ?></td>
+                  <?php } ?>
+                  <?php if (strpos($user->wrdsb_website_url, 'wrdsb.ca') !== FALSE) { ?>
+                    <td><a href="<?php echo $user->wrdsb_website_url; ?>"><?php echo $user->wrdsb_website_url; ?></a></td>
+                  <?php } else { ?>
+                    <td>&nbsp;</td>
+                  <?php } ?>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
 
-            // Include the post format-specific content template.
-            get_template_part( 'content', 'page' );
-          endwhile;
-        ?>
-
-			<div class="table-responsive hidden-xs" >
-				<table class="table table-striped table-bordered table-fixed-head">
-					<thead>
-					<tr>
-						<th class="text-left">Name</th>
-						<th class="text-left">Role</th>
-						<th class="text-left">Voicemail</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-						$url = home_url();
-						$parsedUrl = parse_url($url);
-						$host = explode('.', $parsedUrl['host']);
-						$subdomain = $host[0];
-						$json = file_get_contents('http://ec-iappsrv1.wrdsb.ca/api/person/'.$subdomain);
-						// deserialize data from JSON
-						$contacts = json_decode($json);
-						global $phone_num;
-
-						foreach($contacts as $cont) {
-					        if ($cont->job_desc == "Head Custodian") {
-			                    $phone_num = $cont->phone_no;
-			                };
-					    };
-
-						foreach($contacts as $contact) {
-					?>
-						<tr>
-							<td><?php // if ($contact->website_active == "t") { ?>
-				                <a href="http://teachers.wrdsb.ca/<?php echo $contact->username ?>" target="_blank"><?php echo $contact->Name ?>
-				              <?php // } else {
-				                // echo $contact->Name;
-				              // } ?>
-				            </a></strong>
-							</td>
-							<td><em><?php echo $contact->job_desc ?></em></td>
-							<td><?php echo $contact->extension ?></td>
-						</tr>
-					<?php	
-						}
-					?>
-				</tbody>
-				</table>
-			</div>
-
-			<div class="visible-xs">
-				<ul class="table-list">
-					<?php
-						foreach($contacts as $contact) {
-					?>
-					<li>
-						<p><?php // if ($contact->website_active == "t") { ?>
-				                <a href="http://teachers.wrdsb.ca/<?php echo $contact->username ?>" target="_blank"><?php echo $contact->Name ?>
-				              <?php // } else {
-				                // echo $contact->Name;
-				              // } ?>
-				            </a></strong>
-							<br />
-							<em><?php echo $contact->job_desc ?></em><br />
-							<td>Ext: <?php echo $contact->extension ?></p>
-					</li>
-					<?php	
-						}
-					?>
-				</ul>
-			</div>
-		</div>
-	</div>
+        <div class="visible-sm">
+          <ul class="table-list">
+            <?php foreach($user_query->results as $user) { ?>
+              <li>
+                <p>
+                  <?php echo $user->last_name; ?>, <?php echo $user->first_name; ?>
+                  <br /><em><?php echo $user->wrdsb_job_title; ?></em>
+                  <br />Email: <?php echo $user->user_email; ?>
+                  <br />Ext: <?php echo $user->wrdsb_voicemail; ?>
+                  <br />Website: <a href="<?php echo $user->user_url; ?>"><?php echo $user->user_url; ?></a>
+                </p>
+              </li>
+            <?php } ?>
+          </ul>
+        </div>
+      <?php
+      } else {
+	echo '<p>We are currently building our staff list for the new school year.</p>';
+      } ?>
+    </div>
+  </div>
 </div>
-
 <?php get_footer(); ?>
