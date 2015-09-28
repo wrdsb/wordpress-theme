7ed0deb5284d5906c26a23ea9c3f5af6e76ec45a
Suzanne <suzannezc@users.noreply.github.com>
2015-09-28 10:38:13 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -70,7 +70,7 @@
               <p class="copyright">A WRDSB-ITS Solution</p>
             </div>
             <div class="col-sm-8">
-              <p class="copyright">
+              <p class="copyright aligncenter">
                 <?php wp_loginout(); ?> to <?php echo get_bloginfo('name'); ?>
                 &nbsp;&nbsp; | &nbsp;&nbsp;Go to 
                 <a href="http://staff.wrdsb.ca">Staff Website</a>
