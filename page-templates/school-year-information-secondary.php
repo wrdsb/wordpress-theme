56f29ca196650aa4379c21cfcc763b16d77c6268
Suzanne <suzannezc@users.noreply.github.com>
2015-10-06 09:57:55 -0400

    updating columns

--- page-templates/school-year-information-secondary.php
+++ page-templates/school-year-information-secondary.php
@@ -52,7 +52,7 @@ Template Name: SYI - Secondary
 
     # Just right sidebar
     elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-8">';
+      echo '<div class="col-sm-9">';
 
     # No sidebars
     elseif (($has_left == FALSE) and ($has_right == FALSE)):
@@ -234,7 +234,7 @@ Template Name: SYI - Secondary
 
     # Just right sidebar
     elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-4"">';
+      echo '<div class="col-sm-3">';
       if (!is_front_page()) {
         get_sidebar('rmenu');
       }
