c1b3674bc2ff2b1c2c8e7dd58284586bedf5e0ec
Suzanne <suzannezc@users.noreply.github.com>
2015-10-13 11:12:53 -0400

    Remove hidden links

--- sidebar-right.php
+++ sidebar-right.php
@@ -8,21 +8,5 @@
 */
 ?>
 <?php if ( is_active_sidebar( 'sidebar-right' ) ) : ?>
-  <!--
-  <div class="list-group">
-    <a href="#" class="list-group-item active">
-      <h4 class="list-group-item-heading">School Finder</h4>
-      <p class="list-group-item-text">Find school near your home</p>
-    </a>
-    <a href="#" class="list-group-item">
-      <h4 class="list-group-item-heading">School Calendar</h4>
-      <p class="list-group-item-text">Elementary and Secondary School Calendars</p>
-    </a>
-    <a href="#" class="list-group-item">
-      <h4 class="list-group-item-heading">School Bus</h4>
-      <p class="list-group-item-text">Bus delays and cancel</p>
-    </a>
-  </div>
-  -->
   <?php dynamic_sidebar( 'sidebar-right' ); ?>
 <?php endif; ?>
