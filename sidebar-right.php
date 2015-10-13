424b9ef5ea32061322395dba30a271906c179abe
Suzanne <suzannezc@users.noreply.github.com>
2015-10-13 11:13:54 -0400

    Adding div for sidebar-right

--- sidebar-right.php
+++ sidebar-right.php
@@ -8,5 +8,7 @@
 */
 ?>
 <?php if ( is_active_sidebar( 'sidebar-right' ) ) : ?>
+  <div class="sidebar-right widget-area" role="complementary">
   <?php dynamic_sidebar( 'sidebar-right' ); ?>
+  </div>
 <?php endif; ?>
