2026f4725836461dfb2ad66a5a2d494e8b090950
Suzanne <suzannezc@users.noreply.github.com>
2015-10-16 08:39:51 -0400

    Update home.php

--- home.php
+++ home.php
@@ -8,9 +8,9 @@
     $has_right = FALSE;
     if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;}
     if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;}
-    if (is_front_page() && wrdsb_i_am_a_school()) {
+    if (is_front_page() && !wrdsb_i_am_a_corporate_site()) {
       $has_left = TRUE;
-      }
+    }
 
     # Both sidebars
     # left column
