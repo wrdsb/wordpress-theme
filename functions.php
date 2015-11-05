392c73d5336c808525ed0b1233c841ad60faa4df
Suzanne <suzannezc@users.noreply.github.com>
2015-11-05 13:51:59 -0500

    change the text for lost password

--- functions.php
+++ functions.php
@@ -820,9 +820,20 @@ function my_login_logo_url() {
 }
 add_filter( 'login_headerurl', 'my_login_logo_url' );
 
+// Change the Lost Password Text from the WordPress Login
+function change_lostpassword_text ( $text ) {  
+     $lostpassword_words = "Lost your password?";
+     if ($lostpassword_newtext === $lostpassword_words){$lostpassword_newtext = 'Forgot your password? Change it with MyPassword!';}
+        return $lostpassword_newtext;
+     }
+
+add_filter( 'gettext', 'remove_lostpassword_text' );  
+/* redirecting the lost password link to My Password */
+
 add_filter( 'lostpassword_url', 'my_lost_password_page', 10, 2 );
 function my_lost_password_page( $lostpassword_url, $redirect ) {
     //return home_url( 'https://mypassword.wrdsb.ca/?redirect_to=' . $redirect );
     $lostpassword_url = 'https://mypassword.wrdsb.ca/';
     return $lostpassword_url;
 }
+
