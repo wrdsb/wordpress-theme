f8ae3247a326dbd73f4e071cfbebd2559980ee77
Suzanne <suzannezc@users.noreply.github.com>
2015-11-16 10:44:29 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 
       <h1>404 Page Not Found</h1>
       <p>The information you were looking for may have been moved, deleted or doesn't exist.</p>
-      <p>Please search to find the information, or go back to our <a href="/">home page</a>.</p>
+      <p>Please use the navigation above or search to find the information. If you still cannot find the information, please use the <a href="#contactinformation">Website Feedback Form</a> to let us know what's missing.</p>
       <?php get_search_form(); ?>
 
     </div>
