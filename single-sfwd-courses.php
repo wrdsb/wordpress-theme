568c2e6873702f204e59e6793fb19a8a462422c2
James Schumann <jschumann@gmail.com>
2016-06-07 09:53:29 -0400

    remove categories for courses

--- single-sfwd-courses.php
+++ single-sfwd-courses.php
@@ -68,41 +68,6 @@
         <h1><?php the_title(); ?></h1>
 
         <p><?php the_content(__('Read more'));?></p>
-
-<?php
-	$igc=0;
-	foreach((get_the_category()) as $category) {
-	    if (strtolower($category->cat_name) != 'uncategorized') {
-			$igc = 1;
-		}
-	}
-	if ($igc == 1) {
-		$display_cats = 1;
-	}
-	$number_of_tags = count(get_the_tags());
-	//if ($number_of_tags > 0) {
-	if (get_the_tags()) {
-		$display_tags = 1;
-	}
-	if (!isset($display_cats) && isset($display_tags)) {
-		echo '<div class="clearfix"></div>';
-		echo '<p class="categories gray-dark small">Tags: ';
-                the_tags('',' &middot; ','');
-                echo '</p>';
-	} elseif (isset($display_cats) && !isset($display_tags)) {
-		echo '<div class="clearfix"></div>';
-		echo '<p class="categories gray-dark small">Categories: ';
-                the_category(' &middot; ');
-                echo '</p>';
-	} elseif (isset($display_cats) && isset($display_tags)) {
-		echo '<div class="clearfix"></div>';
-		echo '<p class="categories gray-dark small">Categories: ';
-                the_category(' &middot; ');
-                echo ' Tags: ';
-                the_tags('',' &middot; ','');
-                echo '</p>';
-	}
-?>
   <?php endwhile; else: ?>
   <p> <?php _e('Sorry, no posts matched your criteria.'); ?> </p>
 <?php endif; ?>
