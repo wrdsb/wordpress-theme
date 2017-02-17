c269806c9179d760f0833dfe88b3344f5d375821
James Schumann <jschumann@gmail.com>
2017-02-17 13:44:13 -0500

    list child pages if page has no content

--- content-page.php
+++ content-page.php
@@ -5,7 +5,30 @@
 ?>
 <?php the_title('<h1>', '</h1>'); ?>
 <?php
-  the_content();
+  if ($post->post_content == '') {
+    $children = get_pages(array(
+      'sort_order' => 'ASC',
+      'sort_column' => 'menu_order',
+      'hierarchical' => 0,
+      'parent' => $post->ID,
+      'post_type' => 'page',
+    ));
+    // TODO: Make this a DL
+    if ($children) {
+      echo '<ul>';
+        foreach ($children as $child) {
+          echo '<li>';
+          echo '<strong><a href="'.get_permalink($child->ID).'">'.$child->post_title.'</a></strong>';
+          echo '<p>';
+            echo get_the_excerpt($child);
+          echo '</p>';
+          echo '</li>';
+        }
+      echo '</ul>';
+    }
+  } else {
+    the_content();
+  }
 ?>
 <?php
 	$igc=0;
