f2db8f6f9cd277c92ed4f482b747f3d79de52f16
suzannezc <suzanne_carter@googleapps.wrdsb.ca>
2017-04-11 15:16:15 -0400

    CHI postal code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.5
++ postal code for CHI
+
 ## 1.0.4
 + Safe, Caring & Inclusive Schools Survey Button
 
--- page-templates/school_page.php
+++ page-templates/school_page.php
@@ -58,7 +58,7 @@ Template Name: School List
 						?>
 						<tr>
 							<td><strong><a href="<?php echo $website; ?>" target="_blank"><?php echo $school->full_name; ?></a></strong><br />
-								<?php echo $school->street_address .'<br />'.$school->city.' ON '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>'; ?>
+								<?php echo $school->street_address .'<br />'.$school->city.' ON  '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>'; ?>
 							</td>
 							<td>
 								<!-- <a href="mailto:<?php echo $code; ?>@wrdsb.on.ca"><?php echo $code; ?>@wrdsb.on.ca</a><br />-->
@@ -100,7 +100,7 @@ Template Name: School List
 						?>
 						<tr>
 							<td><strong><a href="<?php echo $website; ?>" target="_blank"><?php echo $school->full_name; ?></a></strong><br />
-								<?php echo $school->street_address .'<br />'.$school->city.' ON '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>'; ?>
+								<?php echo $school->street_address .'<br />'.$school->city.' ON  '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>'; ?>
 							</td>
 							<td>
 								<!-- <a href="mailto:<?php echo $code; ?>@wrdsb.on.ca"><?php echo $code; ?>@wrdsb.on.ca</a><br />-->
@@ -126,24 +126,24 @@ Template Name: School List
 					<tbody>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/alternative-education/choices/" target="_blank">Choices for Youth</a> Cambridge</strong><br>
-								256 Hespeler Road<br>Cambridge ON N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span></td>
+								256 Hespeler Road<br>Cambridge ON  N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td><!-- <a href="mailto:wpk@wrdsb.on.ca">wpk@wrdsb.on.ca</a><br />-->
 								519-622-9041</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/alternative-education/choices/" target="_blank">Choices for Youth</a> Waterloo</strong><br>
-								151 Weber St S<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+								151 Weber St S<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td><!-- <a href="mailto:wpk@wrdsb.on.ca">wpk@wrdsb.on.ca</a><br />-->
 								519-885-0800</td>
 						</tr>
 							<td><strong><a href="https://www.wrdsb.ca/alternative-education/u-turn/" target="_blank">UTurn</a> Cambridge</strong><br>
-								256 Hespeler Road<br>Cambridge ON N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span></td>
+								256 Hespeler Road<br>Cambridge ON  N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td><!-- <a href="mailto:wpk@wrdsb.on.ca">wpk@wrdsb.on.ca</a><br />-->
 								519-622-9041</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/alternative-education/u-turn/" target="_blank">UTurn</a> Waterloo</strong><br>
-								151 Weber St S<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+								151 Weber St S<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td><!-- <a href="mailto:wpk@wrdsb.on.ca">wpk@wrdsb.on.ca</a><br />-->
 								519-885-0800</td>
 						</tr>
@@ -163,27 +163,27 @@ Template Name: School List
 					<tbody>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/continuing-education/" target="_blank">Main Office</a></strong><br>
-							151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+							151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>519-885-0800</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/continuing-education/night-school/" target="_blank">Night School</a></strong><br>
-							151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+							151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>519-885-0800</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/continuing-education/summer-school/" target="_blank">Summer School</a></strong><br>
-							151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+							151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>519-885-0800</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/continuing-education/international-language/" target="_blank">International Languages</a></strong><br>
-							151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+							151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>519-885-0800</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://www.wrdsb.ca/esu/" target="_blank">Essential Skills Upgrading</a></strong><br>
-							151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
+							151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>519-885-4356</td>
 						</tr>
 					</tbody>
@@ -203,12 +203,12 @@ Template Name: School List
 					<tbody>
 						<tr>
 							<td><strong><a href="https://chi.wrdsb.ca/" target="_blank">Chicopee Hills Public School</a></strong><br>
-							800 Fairway Road North<br>Kitchener ON <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=800 Fairway North+Kitchener+Ontario" target="_blank">MAP</a> ]</span></td>
+							800 Fairway Road North<br>Kitchener ON  N2A 0J6<span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=800 Fairway North+Kitchener+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>N/A</td>
 						</tr>
 						<tr>
 							<td><strong><a href="https://gro.wrdsb.ca/" target="_blank">Groh Public School</a></strong><br>
-							225 Thomas Slee Drive<br>Kitchener ON N2P 0B8 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=225 Thomas Slee Drive+Kitchener+Ontario" target="_blank">MAP</a> ]</span></td>
+							225 Thomas Slee Drive<br>Kitchener ON  N2P 0B8 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=225 Thomas Slee Drive+Kitchener+Ontario" target="_blank">MAP</a> ]</span></td>
 							<td>N/A</td>
 						</tr>
 					</tbody>
@@ -239,7 +239,7 @@ Template Name: School List
 					{
 				?>
 				<p style="margin-bottom: 25px;"><strong><a href="<?php echo $website; ?>" target="_blank"><?php echo $school->full_name ?></a></strong></br />
-					<?php echo $school->street_address.'<br />'.$school->city.' ON '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>';?><br />
+					<?php echo $school->street_address.'<br />'.$school->city.' ON  '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>';?><br />
 					<!-- <a href="mailto:<?php echo $code; ?>@wrdsb.on.ca"><?php echo $code; ?>@wrdsb.on.ca</a><br /> -->
 					<?php echo $phone; ?></p>
 				<?php
@@ -264,7 +264,7 @@ Template Name: School List
 					{
 				?>
 					<p style="margin-bottom: 25px;"><strong><a href="<?php echo $website; ?>" target="_blank"><?php echo $school->full_name ?></a></strong></br />
-							<?php echo $school->street_address.'<br />'.$school->city.' ON '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>';?><br />
+							<?php echo $school->street_address.'<br />'.$school->city.' ON  '.$postal_code . ' <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&hl=en&q='.$school->street_address.'+'.$school->city.'+Ontario" target="_blank">MAP</a> ]</span>';?><br />
 							<!-- <a href="mailto:<?php echo $code; ?>@wrdsb.on.ca"><?php echo $code; ?>@wrdsb.on.ca</a><br /> -->
 							<?php echo $phone; ?></p>
 				<?php
@@ -274,44 +274,44 @@ Template Name: School List
 			<h2 id="alt-con-ed_xs">Alternative Education</h2>
 			
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/alternative-education/choices/" target="_blank">Choices for Youth</a> Cambridge</strong><br>
-			256 Hespeler Road<br>Cambridge ON N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span><br>
+			256 Hespeler Road<br>Cambridge ON  N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span><br>
 			519-662-9041</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/alternative-education/choices/" target="_blank">Choices for Youth</a> Waterloo</strong><br>
-			151 Weber St S<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>
+			151 Weber St S<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>
 			519-885-0123</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/alternative-education/u-turn/" target="_blank">UTurn</a> Cambridge</strong><br>
-			256 Hespeler Road<br>Cambridge ON N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span><br>
+			256 Hespeler Road<br>Cambridge ON  N1R 3H3 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=256 Hespeler Road+Cambridge+Ontario" target="_blank">MAP</a> ]</span><br>
 			519-662-9041</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/alternative-education/u-turn/" target="_blank">UTurn</a> Waterloo</strong><br>
-			151 Weber St S<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>
+			151 Weber St S<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St S+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>
 			519-885-0123</p>
 	
 			<h2 id="alt-con-ed_xs">Continuing Education</h2>
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/continuing-education/" target="_blank">Main Office</a></strong><br>
-			151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
+			151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/continuing-education/night-school/" target="_blank">Night School</a></strong><br>
-			151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
+			151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/continuing-education/summer-school/" target="_blank">Summer School</a></strong><br>
-			151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
+			151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/continuing-education/international-language/" target="_blank">International Languages</a></strong><br>
-			151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
+			151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-0800</p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://www.wrdsb.ca/esu/" target="_blank">Essential Skills Upgrading</a></strong><br>
-			151 Weber St. S.<br>Waterloo ON N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-4356</p>
+			151 Weber St. S.<br>Waterloo ON  N2J 2A9 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=151 Weber St. S.+Waterloo+Ontario" target="_blank">MAP</a> ]</span><br>519-885-4356</p>
 
 			<h2 id="new_schools_xs">New Schools Opening in 2017</h2>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://chi.wrdsb.ca/" target="_blank">Chicopee Hills Public School</a></strong><br>
-			800 Fairway Road North<br>Kitchener ON <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=800 Fairway North+Kitchener+Ontario" target="_blank">MAP</a> ]</span></p>
+			800 Fairway Road North<br>Kitchener ON  N2A 0J6<span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=800 Fairway North+Kitchener+Ontario" target="_blank">MAP</a> ]</span></p>
 
 			<p style="margin-bottom: 25px;"><strong><a href="https://gro.wrdsb.ca/" target="_blank">Groh Public School</a></strong><br>
-			225 Thomas Slee Drive<br>Kitchener ON N2P 0B8 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=225 Thomas Slee Drive+Kitchener+Ontario" target="_blank">MAP</a> ]</span></p>
+			225 Thomas Slee Drive<br>Kitchener ON  N2P 0B8 <span class="smallcaps">[ <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=225 Thomas Slee Drive+Kitchener+Ontario" target="_blank">MAP</a> ]</span></p>
 
 		</div>
 	</div>
