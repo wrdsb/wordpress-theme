f2a5b3662dc5b5296f384251655f8067be8b03e4
suzannezc <suzanne_carter@googleapps.wrdsb.ca>
2017-04-13 15:00:02 -0400

    fixing if statement

--- home.php
+++ home.php
@@ -16,7 +16,6 @@
       echo '<div class="col-sm-3 col-md-2 col-lg-2" role="complementary">';
 
       if (is_front_page() && wrdsb_i_am_a_school_exception()) { ?>
-        <p><a href="https://www.wrdsb.ca/blog/2017/04/03/safe-caring-inclusive-school-surveys-for-parentsguardians/" target="_blank" javascript="ga('send', 'event', 'school-banners', 'click-banner', 'SCIS Survey 2017');"><img src="https://www.wrdsb.ca/wp-content/uploads/scis_button2.png" alt="Take our SCIS survey until May 12, 2017" /></a></p>
         <p><a href="https://myway.wrdsb.ca/" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'myway', 'https://myway.wrdsb.ca/',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a></p>
       <?php 
       }
@@ -32,7 +31,6 @@
       }
 
       if (is_front_page() && wrdsb_i_am_a_school()) { ?>
-        <p><a href="https://www.wrdsb.ca/blog/2017/04/03/safe-caring-inclusive-school-surveys-for-parentsguardians/" target="_blank" javascript="ga('send', 'event', 'school-banners', 'click-banner', 'SCIS Survey 2017');"><img src="https://www.wrdsb.ca/wp-content/uploads/scis_button2.png" alt="Take our SCIS survey until May 12, 2017" /></a></p>
         <p><a href="https://myway.wrdsb.ca/" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'myway'"><img src="https://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a></p>
         <p><a href="<?php echo site_url();?>/about/school-year-information" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'syi', '/about/school-year-information',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wp-content/uploads/schoolyearinformation_344x100.jpg" alt="School Year Information Logo"/></a></p>
         <p><a href="https://www.canadahelps.org/dn/15506" target="_blank" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'wefi', 'https://www.canadahelps.org/dn/15506',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wefi/wp-content/uploads/sites/19/wefi_donate.jpg" alt="Donate Online! Waterloo Education Foundation Inc. (WEFI)"></a></p>
@@ -42,6 +40,7 @@
       }
 
       get_sidebar('left');
+
 
     if (is_front_page() && wrdsb_i_am_a_school_exception()) { ?>
       <p><a href="https://www.canadahelps.org/dn/15506" target="_blank" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'wefi', 'https://www.canadahelps.org/dn/15506',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wefi/wp-content/uploads/sites/19/wefi_donate.jpg" alt="Donate Online! Waterloo Education Foundation Inc. (WEFI)"></a></p>
@@ -56,7 +55,6 @@
       echo '<div class="col-sm-3 col-lg-3" role="complementary">';
 
       if (is_front_page() && wrdsb_i_am_a_school_exception()) { ?>
-        <p><a href="https://www.wrdsb.ca/blog/2017/04/03/safe-caring-inclusive-school-surveys-for-parentsguardians/" target="_blank" javascript="ga('send', 'event', 'school-banners', 'click-banner', 'SCIS Survey 2017');"><img src="https://www.wrdsb.ca/wp-content/uploads/scis_button2.png" alt="Take our SCIS survey until May 12, 2017" /></a></p>
         <p><a href="https://myway.wrdsb.ca/" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'myway', 'https://myway.wrdsb.ca/',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a></p>
       <?php 
       }
@@ -72,7 +70,6 @@
       }
 
       if (is_front_page() && wrdsb_i_am_a_school()) { ?>
-        <p><a href="https://www.wrdsb.ca/blog/2017/04/03/safe-caring-inclusive-school-surveys-for-parentsguardians/" target="_blank" javascript="ga('send', 'event', 'school-banners', 'click-banner', 'SCIS Survey 2017');"><img src="https://www.wrdsb.ca/wp-content/uploads/scis_button2.png" alt="Take our SCIS survey until May 12, 2017" /></a></p>
         <p><a href="https://myway.wrdsb.ca/" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'myway'"><img src="https://www.wrdsb.ca/wp-content/uploads/myway_banner_344x100.jpg" alt="MyWay Logo"/></a></p>
         <p><a href="/about/school-year-information" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'syi');"><img src="https://www.wrdsb.ca/wp-content/uploads/schoolyearinformation_344x100.jpg" alt="School Year Information Logo"/></a></p>
         <p><a href="https://www.canadahelps.org/dn/15506" target="_blank" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'wefi', 'https://www.canadahelps.org/dn/15506',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wefi/wp-content/uploads/sites/19/wefi_donate.jpg" alt="Donate Online! Waterloo Education Foundation Inc. (WEFI)"></a></p>
@@ -81,14 +78,15 @@
       <?php
       }
 
-    get_sidebar('left');
- 
+      get_sidebar('left');
+    
       if (is_front_page() && wrdsb_i_am_a_school_exception()) { ?>
          <p><a href="https://www.canadahelps.org/dn/15506" target="_blank" onclick="ga('send', 'event', 'schoolBanners', 'click_banner', 'wefi', 'https://www.canadahelps.org/dn/15506',{'nonInteraction':1});"><img src="https://www.wrdsb.ca/wefi/wp-content/uploads/sites/19/wefi_donate.jpg" alt="Donate Online! Waterloo Education Foundation Inc. (WEFI)"></a></p>
       <?php 
       }
 
-    echo '</div>';
+
+      echo '</div>';
     # Just right sidebar
     elseif (($has_left === FALSE) and ($has_right === TRUE)):
       # Nothing to do
@@ -96,7 +94,9 @@
     elseif (($has_left === FALSE) and ($has_right === FALSE)):
       # Nothing to do
     endif
+    ?>
 
+    <?php
     # Both sidebars
     # content area
     if (($has_left === TRUE) and ($has_right === TRUE)):
