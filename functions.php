9fca0f48edf5f7b3749ac26fa7af4a92c761ed93
Suzanne Carter <suzanne_carter@wrdsb.ca>
2019-10-02 12:18:21 -0400

    change asset version for ui

--- functions.php
+++ functions.php
@@ -412,7 +412,7 @@ require get_template_directory() . '/inc/customizer.php';
 if ( ! function_exists ('wrdsb_global_vars' ) ) {
 function wrdsb_global_vars() {
   global $wrdsbvars;
-  $wrdsbvars['asset_version'] = "1/1.0.3";
+  $wrdsbvars['asset_version'] = "public/2/2.0.0";
   // to use: $GLOBALS['wrdsbvars']['asset_version']
 }
 add_action ( 'parse_query','wrdsb_global_vars' );
