a841d4cf19174cd4f54b823d7a00f9943505bcf0
Suzanne Carter <suzanne_carter@wrdsb.ca>
2021-02-11 08:15:02 -0500

    Update content_corp.php

--- content_corp.php
+++ content_corp.php
@@ -36,21 +36,41 @@
 	<p>cards</p>
 </div>
 
+<div class="clearfix"></div>
+
 <div style="width: 50%; float: left; margin-bottom: 50px;">
-	<h2 style="text-transform: uppercase;">Twitters</h2>
-TWEETS
+	<a class="twitter-timeline" data-width="450" data-height="300" data-theme="light" href="https://twitter.com/wrdsb?ref_src=twsrc%5Etfw">Tweets by wrdsb</a>
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
 
 <div style="width: 50%; float: left; margin-bottom: 50px;">
-	<h2 style="text-transform: uppercase;">Book of faces</h2>
-FACEBOOK
+<div class="fb-page" data-href="https://www.facebook.com/wrdsb" data-tabs="timeline" data-width="450" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/wrdsb" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wrdsb">Waterloo Region District School Board (WRDSB)</a></blockquote></div>
 </div>
+
+<div class="clearfix"></div>
 
 <h2 style="text-transform: uppercase;">Upcoming Events</h2>
 
-<p>Google agenda view</p>
+<iframe src="https://calendar.google.com/calendar/embed?
+showTitle=0
+&amp;showTz=0
+&amp;src=pp2nfhd2jnee8dfvvgqhmfd374%40group.calendar.google.com
+&amp;src=googleapps.wrdsb.ca_8vcbnsnqpr9frj95jshoh47bvk%40group.calendar.google.com
+&amp;src=oerihuqbvr58ajakvcvi6meqq8%40group.calendar.google.com&amp;src=col0r844ggoqm9nvgrl0lr43vk%40group.calendar.google.com
+&amp;src=g5ibshq9uj6lrs97rq8l9mhprk%40group.calendar.google.com
+&amp;showNav=0
+&amp;showDate=0
+&amp;showPrint=0
+&amp;showTabs=0
+&amp;showCalendars=0
+&amp;mode=AGENDA
+&amp;height=300
+&amp;wkst=1
+&amp;bgcolor=%23FFFFFF
+&amp;ctz=America%2FToronto" 
+style="border: 0" width="100%" height="300" frameborder="0" scrolling="no"></iframe>
 
-<p><a href="">See all events</a></p>
+<p><a href="https://www.wrdsb.ca/about-the-wrdsb/calendar/">See the full calendar.</a></p>
 
         </div>
 </div>
